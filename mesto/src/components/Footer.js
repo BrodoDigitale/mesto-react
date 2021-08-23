fdf16ef0d5cebea2d8b988132701b53370111ffa
Julia Sensi <sensi.julia@gmail.com>
2021-08-23 17:40:33 +0200

    Delete mesto directory

--- mesto/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-    return(
-    <footer className="footer">
-        <p className="footer__copyright">&#169; 2021 Mesto Russia</p>
-    </footer>
-    )
-}
--- mesto/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import Logo from '../images/Logo_mesto.svg'
-export function Header() {
-    return(
-    <header className="header">
-        <img className="header__logo" src={Logo} alt="логотип Место" />
-    </header>
-    )
-}
